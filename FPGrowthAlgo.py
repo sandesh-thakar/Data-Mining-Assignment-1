7f9c1ca730a29cc6e9a99c46c18fb75c443979ab
Kunal Verma <39799230+Stumblef00l@users.noreply.github.com>
2019-02-16 10:57:08 +0530

    Working FP Growth along with csv for support value = 1%
    FP Growth now generates all subsets

--- FPGrowthAlgo.py
+++ FPGrowthAlgo.py
@@ -6,6 +6,7 @@
 
 import math as mt
 import pandas as pd
+import csv
 
 #List of frequent itemsets
 
@@ -14,7 +15,7 @@ frequent_itemsets = {}
 #FP Tree's node data type
 
 class FPTreeNode(object):
-    def __init__(self,item_name: str, item_value: float):
+    def __init__(self,item_name: str, item_value: int):
         self.name = item_name
         self.value = item_value
         self.children = []
@@ -40,90 +41,105 @@ def add(root,record):
 #Depth First Search to mine frequent itemsets
 
 def dfs(node,current_list,support_count: int,total_elements: float):
-    going_down = False
     if node.name != "*":
         if node.name not in current_list:
             current_list.append(node.name)
     for i in node.children:
         if i.value >= support_count:
-            new_list = current_list
-            dfs(i,new_list,support_count,total_elements)
-            going_down = True
-    if going_down == False:
-        if len(current_list)!=0:
-            temp_list = current_list
-            tmp = tuple(temp_list)
-            val = node.value
-            support_value = val/total_elements
-            print(support_value)
+            current_list.append(i.name)
+            dfs(i,current_list,support_count,total_elements)
+            current_list.pop()
+    if len(current_list)!=0:
+        val = node.value
+        support_value = val/total_elements
+        support_value = support_value*100
+        n = len(current_list)
+        for i in range(0,(1<<n)):
+            if (i&((1<<(n-1))))==0:
+                continue
+            temp_list = []
+            for j in range(0,n):
+                if (i&(1<<j)):
+                    temp_list.append(current_list[j])
             temp_list.sort()
-            if tmp not in frequent_itemsets:
-                frequent_itemsets[tmp] = support_count
+            tmp = tuple(current_list)
+            if tmp in frequent_itemsets:
+                frequent_itemsets[tmp] = max(frequent_itemsets[tmp],support_value)
+            else:
+                frequent_itemsets[tmp] = support_value
                 
+
+if __name__ == "__main__":
+    #Importing the dataset
+    
+    dataset =  pd.read_csv("groceries.csv",header = None)
+    
+    #Assigning support and finding support count
+    
+    support = 0.01
+    support_count = support * dataset.shape[0]
+    support_count = mt.ceil(support_count) 
+    
+    #Finding number of frequencies of each item in dataset
+    
+    item_count = {}
+    for i in range(0,dataset.shape[0]):
+        for j in range(0,dataset.shape[1]):
+            if(type(dataset.values[i,j])!=str):
+                break
+            if dataset.values[i,j] in item_count:
+                item_count[dataset.values[i,j]]=item_count[dataset.values[i,j]]+1
+            else:
+                item_count[dataset.values[i,j]]=1
+                
+    #Removing items with frequencies below support count
+    
+    temp_key_list = list(item_count.keys())
+    for i in temp_key_list:
+        if item_count[i] < support_count:
+            item_count.pop(i)
+    
+    #Ordering items in descending order based on count
+                
+    ordered_list_of_items = []
+    for i in item_count:
+        new_item = (item_count[i],i)
+        ordered_list_of_items.append(new_item)
+    ordered_list_of_items.sort()
+    
+    
+    #Ordered transactions
+    
+    ordered_transactions = []
+    for i in range(0,dataset.shape[0]):
+        new_transaction = []
+        for j in reversed(ordered_list_of_items):    
+            for k in range(0,dataset.shape[1]):
+                if(type(dataset.values[i,k])!=str):
+                    break
+                if(j[1] == dataset.values[i,k]):
+                    new_transaction.append(j[1])
+                    break
+        ordered_transactions.append(new_transaction)
+    
+    
+    #Creation of the FP Tree
+    
+    root = FPTreeNode("*",100000)
+    for i in ordered_transactions:
+        if(len(i)>0):
+            add(root,i)
+    
+    #Mining for frequent itemsets
+    
+    current_list = []
+    dfs(root,current_list,support_count,dataset.shape[0])
+    
+    #Writing mined itemsets to Excel sheet
+    
+    with open("support10.csv","w") as output:
+        writer = csv.writer(output)
+        writer.writerow(["Frequent Itemset", "Support Percent"])
+        for key,value in frequent_itemsets.items():
+            writer.writerow([key,value])
         
-#Importing the dataset
-
-dataset =  pd.read_csv("groceries.csv",header = None)
-
-#Assigning support and finding support count
-
-support = 0.03
-support_count = support * dataset.shape[0]
-support_count = mt.ceil(support_count) 
-
-#Finding number of frequencies of each item in dataset
-
-item_count = {}
-for i in range(0,dataset.shape[0]):
-    for j in range(0,dataset.shape[1]):
-        if(type(dataset.values[i,j])!=str):
-            break
-        if dataset.values[i,j] in item_count:
-            item_count[dataset.values[i,j]]=item_count[dataset.values[i,j]]+1
-        else:
-            item_count[dataset.values[i,j]]=1
-            
-#Removing items with frequencies below support count
-
-temp_key_list = list(item_count.keys())
-for i in temp_key_list:
-    if item_count[i] < support_count:
-        item_count.pop(i)
-
-#Ordering items in descending order based on count
-            
-ordered_list_of_items = []
-for i in item_count:
-    new_item = (item_count[i],i)
-    ordered_list_of_items.append(new_item)
-ordered_list_of_items.sort()
-
-
-#Ordered transactions
-
-ordered_transactions = []
-for i in range(0,dataset.shape[0]):
-    new_transaction = []
-    for j in reversed(ordered_list_of_items):    
-        for k in range(0,dataset.shape[1]):
-            if(type(dataset.values[i,k])!=str):
-                break
-            if(j[1] == dataset.values[i,k]):
-                new_transaction.append(j[1])
-                break
-    ordered_transactions.append(new_transaction)
-
-
-#Creation of the FP Tree
-
-root = FPTreeNode("*",100000)
-for i in ordered_transactions:
-    if(len(i)>0):
-        add(root,i)
-
-#Mining for frequent itemsets
-
-current_list = []
-dfs(root,current_list,support_count,dataset.shape[0])
-for i in frequent_itemsets:
-    print(i)
